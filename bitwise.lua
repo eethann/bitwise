810a237ec0a21ed51471ed7262e54421cd25ecc6
Ethan Winn <ethan@colab.coop>
2022-07-01 16:23:03 -0400

    UX rework - enc 2 selects control
    [WIP] UX refactor - gates page enc only
    
    WIP UX update, gates mode keys
    
    [WIP] Updated UX key and enc for notes page

--- bitwise.lua
+++ bitwise.lua
@@ -1,10 +1,22 @@
 -- BITWISE
+--
+-- TODO
+-- * fix byte modify ops on notes page
+-- * implement probability
+-- * replace most state vars with params
+-- * tick highlight arg and gates
+-- * title all pages
+-- * Check all funcs against docs
+-- * POST
+-- * EC: implement grid
+--
 -- A bit-logic based sequencer intedned for live play.
 --
 -- ALL PAGES
 -- > k1: exit
 -- > e1: select page
 -- > e2: select control
+-- > + k1: adjust control probability
 --
 -- GATES PAGE controls
 -- > OPERATOR:
@@ -13,12 +25,11 @@
 -- >> e3: l/r rotate both
 -- >> [+ k1]:[oposite directions]
 -- > GATE BYTE:
--- >> k2 [+ k1]: not [momentary] 
--- >> k3 [ + k1]: reflect [momentary]
+-- >> k3 [+ k1]: not [reflect] 
 -- >> e3: +/- value
 -- >> [+ k1]: +/- upper nibble
--- >> [+ k1 k2]: l/r rotate
--- >> [+ k1 k3]: l/r shift
+-- >> [+ k2]: l/r rotate
+-- >> [+ k1 k2]: l/r shift
 --
 -- NOTES PAGE controls
 -- > Each BIT SEQUENCE BYTE
@@ -52,7 +63,7 @@ tick = 0
 opd_gates = 0
 active_mode = 1
 modes = {"gates","notes"}
-focust_control = 1
+focus_control = 1
 
 local function bit_value(n,b)
   return bit32.band(1,bit32.rshift(n,b-1))  
@@ -73,7 +84,7 @@ end
 
 -- TODO separate root_note from base_note so you can have root_note for a scale with a different base note
 function build_scale() 
-  scale = MusicUtil.generate_scale(params:get("root_note"), params:get("scale_mode"),8)
+  scale = MusicUtil.generate_scale(params:get("base_note"), params:get("scale_mode"),8)
 end
 
 function init()
@@ -87,7 +98,7 @@ function init()
   params:set_action("base_note", function(n) base_note = n end)
   base_note = 24
 
-  local scale_names
+  local scale_names = {}
   for i = 1, #MusicUtil.SCALES do
     table.insert(scale_names, string.lower(MusicUtil.SCALES[i].name))
   end
@@ -157,71 +168,113 @@ local function hexfmt(n)
   return string.upper(string.format("%02x",n))
 end
 
-function enc(e, d) --------------- enc() is automatically called by norns
-  if e == 1 then turn(e, d) end -- turn encoder 1
-  if e == 2 then turn(e, d) end -- turn encoder 2
-  if e == 3 then turn(e, d) end -- turn encoder 3
-  screen_dirty = true ------------ something changed
+keys = {} -- track keys depressed at any point
+
+function enc(e, d)
+  turn(e, d)
+  screen_dirty = true
 end
 
-keys = {}
+local function bit8_rrot(n,d)
+  -- if d==1, compliment is -7, if -2 then 6
+  local d_8_compliment = -1 * math.floor(d/math.abs(d)) * (8 - math.abs(d))
+  return bit32.band(0xFF,bit32.rshift(n,d) + bit32.rshift(n,d_8_compliment))
+end
+
+local function bit8_rshift(n,d)
+  return bit32.band(0xFF,bit32.rshift(n,d))
+end
+
+local function bit8_bnot(n)
+  return bit32.band(0xFF,bit32.bnot(n))
+end
 
 function turn(e, d) ----------------------------- an encoder has turned
+  screen_dirty = true
   if e == 1 then
     active_mode = util.clamp(active_mode + d,1,2)
+    focus_control = 1
+    redraw()
     return
   end
   if active_mode == 1 then
-    if (keys[1] == 1) then
-      if (e == 2) then
-        if (d > 0) then
-          gates = bit32.band(0xFF,bit32.rshift(gates,1) + bit32.rshift(gates,-7))
-        elseif (d < 0) then
-          gates = bit32.band(0xFF,bit32.rshift(gates,-1) + bit32.rshift(gates,7))
+    if e == 2 then
+      focus_control= util.clamp(focus_control + d,1,3)
+      redraw()
+    elseif e == 3 then
+      if focus_control == 1 then
+        -- if k[1] is down, move arg and gates in op directions
+        local inverse = (keys[1] == 1) and -1 or 1 
+        gates = bit8_rrot(gates,d)
+        arg = bit8_rrot(arg,inverse*d)
+        -- if (d > 0) then
+        --   bit32.band(0xFF,bit32.rshift(gates,1) + bit32.rshift(gates,-7))
+        --   arg = bit32.band(0xFF,bit32.rshift(arg,1*inverse) + bit32.rshift(arg,-7*inverse))
+        -- elseif (d < 0) then
+        --   gates = bit32.band(0xFF,bit32.rshift(gates,-1) + bit32.rshift(gates,7))
+        --   arg = bit32.band(0xFF,bit32.rshift(arg,-1*inverse) + bit32.rshift(arg,7*inverse))
+        -- end
+      else
+        if keys[2] == 1 then
+          -- rot / shift
+          local enc_op = (keys[1] ~= 1) and bit8_rrot or bit8_rshift
+          if focus_control == 2 then
+            gates = enc_op(gates,d)
+          else
+            arg = enc_op(arg,d)
+          end
+        else
+          -- inc byte / most significant nibble
+          local amt = d * ((keys[1] == 1) and 0x10 or 1)
+          if focus_control == 2 then
+            gates = util.wrap(gates+amt,0,256)
+          else
+            arg = util.wrap(arg+amt,0,256)
+          end
         end
-      elseif (e == 3) then
-        if (d > 0) then
-          arg = bit32.band(0xFF,bit32.rshift(arg,1) + bit32.rshift(arg,-7))
-        elseif (d < 0) then
-          arg = bit32.band(0xFF,bit32.rshift(arg,-1) + bit32.rshift(arg,7))
-        end
-      end
-    else
-      if (e == 2) then
-        gates = util.wrap(gates+d,0,256)
-      elseif (e == 3) then
-        arg = util.wrap(arg+d,0,256)
       end
     end
   elseif active_mode == 2 then
     if (e==2) then
-      if (keys[1] == 1) then
-        note_bit_bytes[focust_control] = bit32.band(0xFF,bit32.rshift(note_bit_bytes[focust_control],d))
-      else
-        focust_control = util.clamp(focust_control + d,1,5)
-      end
+      focus_control= util.clamp(focus_control + d,1,4)
+      redraw()
+      -- if (keys[1] == 1) then
+      --   note_bit_bytes[focus_control] = bit32.band(0xFF,bit32.rshift(note_bit_bytes[focus_control],d))
+      -- else
+      --   focus_control = util.clamp(focus_control + d,1,5)
+      -- end
     elseif (e==3) then
-      if (keys[1] == 1) then
-        if (d > 0) then
-          note_bit_bytes[focust_control] = bit32.band(0xFF,bit32.rshift(note_bit_bytes[focust_control],1) + bit32.rshift(note_bit_bytes[focust_control],-7))
-        elseif (d < 0) then
-          note_bit_bytes[focust_control] = bit32.band(0xFF,bit32.rshift(note_bit_bytes[focust_control],-1) + bit32.rshift(note_bit_bytes[focust_control],7))
-        end
+      -- TODO confirm this is a reference
+      local subject = note_bit_bytes[focus_control]
+      if keys[2] == 1 then
+        -- rot / shift depends on k[1]
+        local enc_op = (keys[1] ~= 1) and bit8_rrot or bit8_rshift
+        note_bit_bytes[focus_control]= enc_op(subject,d)
       else
-        note_bit_bytes[focust_control] = util.clamp(note_bit_bytes[focust_control]+d,0,256)
+        -- inc byte / most significant nibble depending on k[1]
+        local amt = d * ((keys[1] == 1) and 0x10 or 1)
+        note_bit_bytes[focus_control]= util.wrap(subject+amt,0,256)
       end
+      -- if (keys[1] == 1) then
+      --   if (d > 0) then
+      --     note_bit_bytes[focus_control] = bit32.band(0xFF,bit32.rshift(note_bit_bytes[focus_control],1) + bit32.rshift(note_bit_bytes[focus_control],-7))
+      --   elseif (d < 0) then
+      --     note_bit_bytes[focus_control] = bit32.band(0xFF,bit32.rshift(note_bit_bytes[focus_control],-1) + bit32.rshift(note_bit_bytes[focus_control],7))
+      --   end
+      -- else
+      --   note_bit_bytes[focus_control] = util.clamp(note_bit_bytes[focus_control]+d,0,256)
+      -- end
     end
   end
 end
 
 function key(k, z)
   keys[k] = z == 1 and 1 or nil
+  -- lazy: always redraw on key action
+  screen_dirty = true
   if active_mode == 1 then
-    if #keys == 0 then
-      -- gate_op = nil
-      -- return
-    end
-    if z == 1 then
+    if focus_control == 1 then
+      -- complex op assignment logic here
       if (keys[1] ~= nil) then
         if (keys[2] ~= nil) then
           gate_op = "nand"
@@ -235,13 +288,25 @@ function key(k, z)
           gate_op = "or"
         end
       end
-      screen_dirty = true
+    else
+      if k == 3 then
+        -- momentary if k[1], toggle otherwise
+        if keys[1] == 1 or z == 1 then
+          -- TODO simplify this logic by storing all state in a table and using reference to assign
+          if focus_control == 2 then
+            gates = bit8_bnot(gates)
+          else
+            arg = bit8_bnot(arg)
+          end
+        else
+          -- TODO implement reflection
+        end
+      end
     end
   elseif active_mode == 2 then
     if z == 1 then
       if k == 2 then
-        note_bit_bytes[focust_control] = bit32.band(0xFF,bit32.bnot(note_bit_bytes[focust_control]))
-        screen_dirty = true
+        note_bit_bytes[focus_control] = bit32.band(0xFF,bit32.bnot(note_bit_bytes[focus_control]))
       end
     end
   end
@@ -262,13 +327,15 @@ function redraw_clock() ----- a clock that draws space
   end
 end
 
-function draw_bin(num,bits,x,y,w,h,hilite_tick)
+function draw_bin(num,bits,x,y,w,h,level,hilite_tick)
+  level = level ~= nil and level or 6
   local xd = w/bits
+  screen.level(level)
   screen.rect(x,y,w,h)
   screen.stroke()
   for i=1,bits do
     if bit32.band(num,2^(bits-i)) ~= 0 then
-      screen.level((hilite_tick == nil or (8 - (tick - 1) % 8) == i) and 15 or 6)
+      screen.level(((hilite_tick ~= nil) and (8 - (tick - 1) % 8) == i) and 15 or level)
       screen.rect(x+(i-1)*xd,y,xd,h)
       screen.fill()
     end
@@ -284,38 +351,40 @@ function redraw()
     screen.level(15) ------------- max
     screen.font_face(1) ---------- set the font face to "04B_03"
     screen.font_size(24) ---------- set the size to 8
-    screen.move(4,24)
-    screen.text(hexfmt(gates))
+    -- screen.move(4,24)
+    -- screen.text(hexfmt(gates))
     screen.move(64,24)
+    screen.level(focus_control == 1 and 8 or 2)
     screen.text_center(op or "--")
-    screen.move(128,24)
-    screen.text_right(hexfmt(arg))
+    -- screen.move(128,24)
+    -- screen.text_right(hexfmt(arg))
     screen.font_size(8) ---------- set the size to 8
     -- screen.move(4,64)
     -- screen.text(tick or "--")
     screen.move(4, 7) ---------- move the pointer to x = 64, y = 32
     screen.text(message) 
     screen.fill() ---------------- fill the termini and message at once
-    draw_bin(gates,8,10,28,106,8)
-    draw_bin(arg,8,10,36,106,8)
-    draw_bin(opd_gates,8,10,54,106,8,true)
+    draw_bin(gates,8,10,28,106,8,focus_control == 2 and 8 or 2,true)
+    draw_bin(arg,8,10,36,106,8,focus_control == 3 and 8 or 2,true)
+    screen.level(15)
+    draw_bin(opd_gates,8,10,54,106,8,6,true)
   elseif (active_mode == 2) then
     -- TODO add hex totals for each step
     screen.font_face(1) ---------- set the font face to "04B_03"
     screen.font_size(8) ---------- set the size to 8
     for i=1,4 do
-      screen.level((focust_control == i) and 15 or 4)
+      screen.level((focus_control == i) and 15 or 4)
       screen.move(1,i*10)
       screen.text(math.floor(2^(i-1)))
-      draw_bin(note_bit_bytes[i],8,10,i*10-5,106,8,true)
+      draw_bin(note_bit_bytes[i],8,10,i*10-5,106,8,focus_control == i and 8 or 2,true)
     end
     for i=1,8 do
       local note = base_note + get_note_bit_bytes_step(9-i)
       screen.move(16+((i-1)*106/8),52)
-      screen.level(8 - ((tick - 1) % 8) == i and 15 or 6)
+      screen.level(8 - ((tick - 1) % 8) == i and 15 or 2)
       screen.text_center(MusicUtil.note_num_to_name(scale[note]))
     end
-    draw_bin(opd_gates,8,10,54,106,8,true)
+    draw_bin(opd_gates,8,10,54,106,8,6,true)
   end
   screen.update() -------------- update space
 end
